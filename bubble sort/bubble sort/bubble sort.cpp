ed4eb30f5774eba988ba1f73e2d769100bd29cec
Gustavo Fagundes <gustavofagundes1998@edu.univali.br>
2018-09-08 16:40:33 -0300

    rqwqewqwerwqr

---
+++ bubble sort/bubble sort/bubble sort.cpp
@@ -0,0 +1,42 @@
+﻿// Bubble Sorte.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include "string"
+#include <iostream>
+#include <stdio.h>
+#include <ctime>
+
+using namespace std;
+const int  tamanho = 4;
+
+int bubble(int valor[], int i) {
+  int aux = 0;
+
+  for (int i = 0; i<tamanho; i++) {
+    for (int x = i + 1; x < tamanho; x++) {
+      if (valor[i] < valor[x]) {
+        aux = valor[i];
+        valor[i] = valor[x];
+        valor[x] = aux;
+      }
+    }
+  }
+
+  return valor[i];
+}
+
+int main()
+{
+  int vetor[tamanho] = { 0,1,3,4 };
+
+  bubble(vetor, tamanho);
+
+  for (int i = 0; i < tamanho; i++) {
+    cout << vetor[i] << " ";
+  }
+
+  cout << endl;
+  system("pause");
+  return 0;
+}

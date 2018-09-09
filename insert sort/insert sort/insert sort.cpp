c2d9eef66a917448a8f32938d97cb1f408edb07f
Gustavo Fagundes <gustavofagundes1998@edu.univali.br>
2018-09-08 22:37:38 -0300

    insert sort test

---
+++ insert sort/insert sort/insert sort.cpp
@@ -0,0 +1,15 @@
+﻿// insert sort.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include "string"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello world"<<endl;
+  system("pause");
+    return 0;
+}
+

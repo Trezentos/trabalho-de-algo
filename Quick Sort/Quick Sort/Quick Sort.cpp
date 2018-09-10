151df80420cca30b5d852637f7da3fa2255aa3ce
Gustavo Fagundes <gustavofagundes1998@edu.univali.br>
2018-09-10 00:32:00 -0300

    Organização do quick sort

--- Quick Sort/Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort/Quick Sort.cpp
@@ -32,19 +32,21 @@ int main() {
   system("pause");
 }
 
-void Quick(int vetor[tamanho], int inicio, int fim) {
-
+void Quick(int vetor[tamanho], int inicio, int fim) { 
+  // A função pega o indice 0 e o ultinmo indice do vetor e o proprio vetor
   int pivo, aux, i, j, meio;
 
   i = inicio;
   j = fim;
-
+  //E com o auxilio de uma equação que soma todos os indices e divide por dois, é encontrado o "pivo", no qual é o 
+  //vetor[indice] do meio, para poder dividir o vetor em dois
   meio = (int)((i + j) / 2);
   pivo = vetor[meio];
 
+  //abaixo o vetor é dividido em dois
   do {
-    while (vetor[i] < pivo) i = i + 1;
-    while (vetor[j] > pivo) j = j - 1;
+    while (vetor[i] < pivo) i = i + 1; //a parte esquerda do vetor, enquanto não estiver organizado, a condição vai ser verdadeira
+    while (vetor[j] > pivo) j = j - 1; //a parte direita do vetor acontece o mesmo caso, até que os dois vetores sejam ordenados
 
     if (i <= j) {
       aux = vetor[i];
@@ -58,5 +60,5 @@ void Quick(int vetor[tamanho], int inicio, int fim) {
   if (inicio < j) Quick(vetor, inicio, j);
   if (i < fim) Quick(vetor, i, fim);
 
-  
+  //e assim o vetor escolhido será organizado com sucesso 
 }

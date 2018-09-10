ecfbae2280cecc9fae9800a2ab2c517691ab775c
Gustavo Fagundes <gustavofagundes1998@edu.univali.br>
2018-09-10 03:05:58 -0300

    Selection Sort

--- Quick Sort/Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort/Quick Sort.cpp
@@ -1,12 +1,17 @@
-﻿#include<stdio.h>
+﻿#include <stdio.h>
 #include "stdafx.h"
 #include "string"
+#include <ctime>
+#include <iostream>
 
-const int tamanho = 10;
+using namespace std;
+
+const int tamanho = 100000; //numero de escolha para o tamanho do vetor...
 void Quick(int vetor[tamanho], int inicio, int fim);
 int main() {
 
   int vetor[tamanho] = { 0 }, contador=0;
+  time_t t;
 
       contador = tamanho;
   for (int indice = 0; indice < tamanho; indice++) {
@@ -14,28 +19,23 @@ int main() {
       vetor[indice] = contador;
       contador--;
   }
+  
+  
 
-  int i;
-  printf("Vetor decrescente:\n");
-  for (i = 0; i < tamanho; i++) {
-    printf("%d ", vetor[i]);
-  }
-  printf("\n");
 
+  t = clock();
   Quick(vetor, 0, tamanho-1); //FUNÇÃO DO QUICK COMEÇANDO
+  t = clock() - t;
 
-  printf("Vetor ordenado crescentemente:\n");
-  for (i = 0; i < tamanho; i++) {
-    printf("%d ", vetor[i]);
-  }
+  cout << "O tempo demorado foi de " << ((float)t)/CLOCKS_PER_SEC << " segundos.";
   printf("\n");
   system("pause");
 }
 
 void Quick(int vetor[tamanho], int inicio, int fim) { 
   // A função pega o indice 0 e o ultinmo indice do vetor e o proprio vetor
-  int pivo, aux, i, j, meio;
-
+  int pivo, aux, i, j, meio, trocas=0;
+  
   i = inicio;
   j = fim;
   //E com o auxilio de uma equação que soma todos os indices e divide por dois, é encontrado o "pivo", no qual é o 
@@ -54,11 +54,12 @@ void Quick(int vetor[tamanho], int inicio, int fim) {
       vetor[j] = aux;
       i = i + 1;
       j = j - 1;
+      trocas++;
     }
   } while (j > i);
-
+  
   if (inicio < j) Quick(vetor, inicio, j);
   if (i < fim) Quick(vetor, i, fim);
-
+  cout << trocas << endl;
   //e assim o vetor escolhido será organizado com sucesso 
 }
---
+++ Selection Sort/Selection Sort/Selection Sort.cpp
@@ -0,0 +1,45 @@
+﻿// Selection Sort.cpp : Define o ponto de entrada para a aplicação de console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "string"
+#include <ctime>
+
+using namespace std;
+
+int main()
+{ 
+  const int tamanho = 1000;
+  int vetor[tamanho] = { tamanho }, i, j, min, x, contador=0;
+  time_t t;
+
+  setlocale(LC_ALL, "portuguese");
+   contador = tamanho;
+  for (int indice = 0; indice < tamanho; indice++) {
+    vetor[indice] = contador;
+    contador--;
+  }
+
+  t = clock();
+  for (i = 0; i <= tamanho-1; i++) { 
+    min = i; //Aqui é selecionado a casa antecessor 
+    for (j = i + 1; j <= tamanho-1; j++) {
+      if (vetor[j] < vetor[min]) //E aqui o sucessor, que vai acessando as proximas casa e comparando com o antecessor
+        min = j;                 //caso a condição seja satisfeita, a troca dos elementos acontece, caso contrário
+    }                            //este que vai comparando com o mínimo vai se comparando com os proximos até que o 
+                                 //vetor esteja totalmente ordenado.
+    x = vetor[min];
+    vetor[min] = vetor[i];
+    vetor[i] = x;
+  }
+  t = clock() - t;
+ 
+  cout << "O tempo em segundos em um vetor de tamanho " << tamanho << " é de  ";
+  cout << ((float)t) / CLOCKS_PER_SEC << " segundos. \n";
+  
+
+    system("pause");
+    return 0;
+}
+
